535570291198e9a046d79f8daec47354046d6311
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-08-13 12:09:43 +0600

    Updated the admin user

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+// use Laravel\Horizon\Horizon;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
@@ -35,7 +35,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                'farhan@laravel-on-beanstalk.site'
+                'farhan@questionboard.test'
             ]);
         });
     }
